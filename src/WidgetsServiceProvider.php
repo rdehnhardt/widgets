8fb9296312b9b77a5ba84c98244913918b248642
Renato Dehnhardt <renatotkd@gmail.com>
2018-03-04 23:19:25 +0100

    Add widget service

--- src/WidgetsServiceProvider.php
+++ src/WidgetsServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace Rdehnhardt\Widgets;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class WidgetsServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Booting
+     */
+    public function boot()
+    {
+        Blade::directive('widget', function ($expression) {
+            return "<?php echo app($expression) ?>";
+        });
+    }
 }
