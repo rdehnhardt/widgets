fef7c09ca817bb4b117a4e7ac5064f040a680db5
Renato Dehnhardt <renatotkd@gmail.com>
2018-03-05 14:52:18 +0100

    Add widget base

--- src/Widget.php
+++ src/Widget.php
@@ -7,38 +7,8 @@ use Illuminate\View\View;
 abstract class Widget
 {
     /**
-     * View name
-     * @var string
-     */
-    protected $view;
-
-    /**
-     * View Data
-     * @var array
-     */
-    protected $data = [];
-
-    /**
      * Handle widget
-     */
-    abstract public function handle();
-
-    /**
-     * @return string
-     * @throws \Throwable
-     */
-    public function __toString()
-    {
-        $this->handle();
-
-        return $this->view()->render();
-    }
-
-    /**
      * @return View
      */
-    private function view()
-    {
-        return view($this->view, $this->data);
-    }
+    abstract public function handle();
 }
--- src/WidgetsServiceProvider.php
+++ src/WidgetsServiceProvider.php
@@ -12,8 +12,12 @@ class WidgetsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('widget', function ($expression) {
-            return "<?php echo app($expression) ?>";
+        $omitParenthesis = version_compare($this->app->version(), '5.3', '<');
+
+        Blade::directive('widget', function ($expression) use ($omitParenthesis) {
+            $expression = $omitParenthesis ? $expression : "($expression)";
+
+            return "<?php echo app($expression)->handle() ?>";
         });
     }
 }
